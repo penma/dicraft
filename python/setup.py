05e3c632afc351c00c3e522ed284d2830898c45f
Lars Stoltenow <penma@penma.de>
2014-09-06 16:22:22 +0200

    fix compile flags for python module

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 setup(name="dicomprint", version="0.1", ext_modules=[
 	Extension("dicomprint",
 		sources = [ "module.c", "binary_image.c", "grayscale_image.c" ],
-		extra_compile_args = [ "-std=gnu99", "-Wall", "-Wextra", "-Wno-unused-parameter", "-Wno-strict-prototypes" ],
+		extra_compile_args = [ "-std=gnu99", "-Wall", "-Wextra", "-Wno-unused-parameter", "-Wno-strict-prototypes", "-Wno-declaration-after-statement" ],
 		libraries = [ "dicomprint" ],
 		include_dirs = [ "../libdicomprint" ],
 		library_dirs = [ "../libdicomprint" ],
