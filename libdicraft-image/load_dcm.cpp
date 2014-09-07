636f23c5fd05d4a1ae73ead0930891a78e69c51a
Lars Stoltenow <penma@penma.de>
2014-09-07 04:17:41 +0200

    DCMTK -> GDCM
    on linux: a bit faster
    on windows: actually builds at all on 64 bit systems

--- libdicraft-image/load_dcm.cpp
+++ libdicraft-image/load_dcm.cpp
@@ -1,52 +1,23 @@
-/* needs at most: -lofstd -ldcm{data,image,imgle,jpeg} */
-
 #include "image3d/grayscale.h"
 #include "load_dcm.h"
 
-#define HAVE_CONFIG_H
-#include "dcmtk/config/osconfig.h"
-#include "dcmtk/dcmdata/dctk.h"
-#include "dcmtk/dcmimgle/dcmimage.h"
-#include "dcmtk/dcmjpeg/djdecode.h"
-#undef HAVE_CONFIG_H
+#include "gdcmImageReader.h"
 
 #include <stdint.h>
 #include <dirent.h>
+#include <unistd.h>
 
 using namespace std;
 
 static int load_single_image(const char *fn, grayscale_t target, const int z) {
-	/* ignored if called multiple times */
-	DJDecoderRegistration::registerCodecs(EDC_photometricInterpretation);
-
-	DcmFileFormat *dfile = new DcmFileFormat();
-	OFCondition status = dfile->loadFile(fn);
-	if (!status.good()) {
-		fprintf(stderr, "Can't read DICOM file %s: %s\n", fn, status.text());
-		return 0;
-	}
-
-	DcmDataset *dataset = dfile->getDataset();
-	E_TransferSyntax xfer = dataset->getOriginalXfer();
-
-	DicomImage *di = new DicomImage(dfile, xfer, 0 /* flags */, 0UL, 1UL);
-	if (di == NULL) {
-		/* OOM? */
-		return 0;
-	}
-
-	if (di->getStatus() != EIS_Normal) {
-		fprintf(stderr, "Error loading image from %s: %s\n", fn, DicomImage::getString(di->getStatus()));
-		return 0;
-	}
-
-	if (!di->isMonochrome()) {
-		fprintf(stderr, "Unexpected image: is not monochrome. Converting currently unsupported\n");
-		return 0;
-	}
-
-	int im_width = di->getWidth();
-	int im_height = di->getHeight();
+	gdcm::ImageReader ir;
+	ir.SetFileName(fn);
+	ir.Read();
+	const gdcm::Image &di = ir.GetImage();
+	
+	const unsigned int *dims = di.GetDimensions();
+	int im_width = dims[0];
+	int im_height = dims[1];
 
 	if ((target->size_x != im_width) || (target->size_y != im_height)) {
 		fprintf(stderr, "Image file dimensions (%dx%d) do not match that of the 3D image (%dx%d)\n",
@@ -61,51 +32,20 @@ static int load_single_image(const char *fn, grayscale_t target, const int z) {
 		return 0;
 	}
 
-	if (! di->getOutputData(
-		target->voxels + z * target->off_z,
-		target->off_z * sizeof(uint16_t),
-		16, 0, 0
-	)) {
-		fprintf(stderr, "Error reading pixel data: %s\n", DicomImage::getString(di->getStatus()));
-		return 0;
-	}
-
-	delete di;
-	delete dfile;
+	di.GetBuffer((char*)(target->voxels + z * target->off_z));
 
 	return 1;
 }
 
 static int dcm_dimensions(const char *fn, int *pwidth, int *pheight) {
-	/* ignored if called multiple times */
-	DJDecoderRegistration::registerCodecs(EDC_photometricInterpretation);
-
-	DcmFileFormat *dfile = new DcmFileFormat();
-	OFCondition status = dfile->loadFile(fn);
-	if (!status.good()) {
-		fprintf(stderr, "Can't read DICOM file %s: %s\n", fn, status.text());
-		return 0;
-	}
-
-	DcmDataset *dataset = dfile->getDataset();
-	E_TransferSyntax xfer = dataset->getOriginalXfer();
-
-	DicomImage *di = new DicomImage(dfile, xfer, 0 /* flags */, 0UL, 1UL);
-	if (di == NULL) {
-		/* OOM? */
-		return 0;
-	}
-
-	if (di->getStatus() != EIS_Normal) {
-		fprintf(stderr, "Error opening image from %s: %s\n", fn, DicomImage::getString(di->getStatus()));
-		return 0;
-	}
-
-	*pwidth = di->getWidth();
-	*pheight = di->getHeight();
-
-	delete di;
-	delete dfile;
+	gdcm::ImageReader ir;
+	ir.SetFileName(fn);
+	ir.Read();
+	const gdcm::Image &di = ir.GetImage();
+	
+	const unsigned int *dims = di.GetDimensions();
+	*pwidth = dims[0];
+	*pheight = dims[1];
 
 	return 1;
 }
@@ -145,3 +85,4 @@ grayscale_t load_dicom_dir(const char *dn) {
 
 	return im;
 }
+
