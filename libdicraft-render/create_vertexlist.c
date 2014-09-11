1cd52abd4a96fb3a16508a4584ea3f947704e1d1
Lars Stoltenow <penma@penma.de>
2014-09-11 03:09:14 +0200

    implemented both functions with memchr directly instead of memchr_off
    Saves storing a few temporaries. The offsets can instead just be calculated
    from the actual pointer and the start-of-line pointer.

--- libdicraft-render/create_vertexlist.c
+++ libdicraft-render/create_vertexlist.c
@@ -13,6 +13,8 @@
 static void cubedim(short xa, short ya, short za, short xb, short yb, short zb, short *vnbuf, int *vertcount) {
 	short *vn = vnbuf + *vertcount * 2 * 3;
 
+	if (xa >= xb){ fprintf(stderr, "LOL\n"); exit(1); }
+
 	short nx, ny, nz;
 #   define V(a,b,c) \
 	vn[0] = (a 1 > 0 ? xb : xa); \
@@ -47,15 +49,6 @@ static void cubedim(short xa, short ya, short za, short xb, short yb, short zb, 
 	*vertcount += 6*2*3;
 }
 
-static inline int memchr_off(const uint8_t *s, uint8_t c, size_t n) {
-	uint8_t *po = memchr(s, c, n);
-	if (po == NULL) {
-		return -1;
-	} else {
-		return po - s;
-	}
-}
-
 /* Append all voxels for this image to the given vertex+normal buffer.
  * Updates the vertex count. Expects the buffer to have enough space already.
  */
@@ -67,28 +60,31 @@ void append_vertices_for_image(binary_t im, short *vnbuf, int *vertcount) {
 
 	for (int z = 0; z < zm; z++) {
 		for (int y = 0; y < ym; y++) {
-			int rest = xm;
 			uint8_t *ptr = im->voxels + binary_offset(im, 0, y, z);
 			uint8_t *pt0 = ptr;
-			int o;
 			while (1) {
-				o = memchr_off(ptr, 0xff, rest);
-				if (o == -1) break;
-				int startx = (ptr + o - pt0);
-				ptr += o + 1;
-				rest -= o + 1;
+				/* Scan for next solid block.
+				 * This will be the xa coordinate of a cube.
+				 */
+				ptr = memchr(ptr, 0xff, pt0 + xm - ptr);
+				if (ptr == NULL) break; /* nothing found on this line ... */
+				int startx = ptr - pt0;
 
-				o = memchr_off(ptr, 0x00, rest);
-				if (o == -1) {
-					/* so this row ended on image boundary.
-					 * generate a cube for it and stop
+				/* Scan for next free block. This will be the xb coordinate.
+				 * What is found is the pos of the next free block
+				 * so it's that of last free block + 1,
+				 * which is exactly where our cube has to end
+				 */
+				ptr = memchr(ptr + 1, 0x00, pt0 + xm - ptr - 1);
+				if (ptr == NULL) {
+					/* No free block found. This means our
+					 * row of solid blocks extends to the
+					 * image boundary.
 					 */
 					cubedim(startx, y, z, xm, y+1, z+1, vnbuf, vertcount);
 					break;
 				} else {
-					cubedim(startx, y, z, startx + 1 + o, y+1, z+1, vnbuf, vertcount);
-					ptr += o + 1;
-					rest -= o + 1;
+					cubedim(startx, y, z, ptr - pt0, y+1, z+1, vnbuf, vertcount);
 				}
 			}
 		}
@@ -107,21 +103,16 @@ int count_vertices(binary_t im) {
 
 	for (int z = 0; z < zm; z++) {
 		for (int y = 0; y < ym; y++) {
-			int rest = xm;
 			uint8_t *ptr = im->voxels + binary_offset(im, 0, y, z);
-			int o;
+			uint8_t *pt0 = ptr;
 			while (1) {
-				o = memchr_off(ptr, 0xff, rest);
-				if (o == -1) break;
-				ptr += o + 1;
-				rest -= o + 1;
+				ptr = memchr(ptr, 0xff, pt0 + xm - ptr);
+				if (ptr == NULL) break;
 
 				vertices += 6*2*3;
 
-				o = memchr_off(ptr, 0x00, rest);
-				if (o == -1) break;
-				ptr += o + 1;
-				rest -= o + 1;
+				ptr = memchr(ptr + 1, 0x00, pt0 + xm - ptr - 1);
+				if (ptr == NULL) break;
 			}
 		}
 	}
